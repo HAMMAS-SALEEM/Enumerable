ca0e6e0b7bb19a8dfdeb9d9c7d1db35a1b52617c
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-04-01 13:03:21 +0500

    Fix Linters

--- enumerable.rb
+++ enumerable.rb
@@ -14,5 +14,4 @@ module MyEnumerable
     each { |item| result.push(item) if yield item }
     result
   end
-
 end
--- my_list.rb
+++ my_list.rb
@@ -14,14 +14,13 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? {|e| e < 5}
+puts(list.all? { |e| e < 5 })
 
-puts list.all? {|e| e > 5}
-
+puts(list.all? { |e| e > 5 })
 
 # Test #any?
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
 # Test #filter
-puts list.filter? {|e| e.even?}
+puts list.filter?(&:even?)
